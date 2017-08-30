72fba947bb5b05b39ba27eba08b65dbe1292e206
Sandro Mani <manisandro@gmail.com>
2017-08-30 17:34:32 +0200

    Rename to kadas_help

--- UserManual/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PLUGIN=UserManual
-GITVERSION=kadas
-
-make package VERSION=$GITVERSION
-scp $PLUGIN.zip pkg@pkg:www/qgis/vbs/
-ssh pkg@pkg qgis-plugin-repo-scan http://pkg.sourcepole.ch/qgis /home/pkg/www/qgis
